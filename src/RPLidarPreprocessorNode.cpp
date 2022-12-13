614977f082c50fe924be4fde124dcf5de90e713c
CharlesXu1124 <xuzheyuan961124@gmail.com>
2022-12-13 03:19:03 -0800

    finished up rplidar laser scan preprocessing package

--- src/RPLidarPreprocessorNode.cpp
+++ src/RPLidarPreprocessorNode.cpp
@@ -3,13 +3,9 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
-#include <image_transport/image_transport.hpp>
 
 #include <memory>
-#include "cv_bridge/cv_bridge.h"
 #include <opencv2/opencv.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/highgui/highgui.hpp>
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
@@ -25,29 +21,23 @@ public:
     scan_subscriber = this->create_subscription<sensor_msgs::msg::LaserScan>(
         "/scan", 10, std::bind(&RPLidarPreprocessNode::scanReceived, this, _1));
 
-    scan_publisher = this->create_publisher<sensor_msgs::msg::CompressedImage>(
+    scan_publisher = this->create_publisher<sensor_msgs::msg::LaserScan>(
         "/scan_processed", 10);
   }
 
 private:
   void scanReceived(sensor_msgs::msg::LaserScan::SharedPtr scan)
   {
-    uint32_t counter = 0;
-    std::vector<float> ranges = scan->ranges;
-    for (int i = 0; i < ranges.size(); ++i)
-    {
-      if (ranges[i] > 100)
-      {
-        counter++;
-      }
-    }
 
-    RCLCPP_INFO(get_logger(), "there are %d empty points", counter);
 
+    scan_publisher->publish(*scan);
+
+    // pause for 20 milliseconds
+    waitKey(20);
   }
 
   rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr scan_subscriber;
-  rclcpp::Publisher<sensor_msgs::msg::CompressedImage>::SharedPtr scan_publisher;
+  rclcpp::Publisher<sensor_msgs::msg::LaserScan>::SharedPtr scan_publisher;
 };
 
 int main(int argc, char **argv)
